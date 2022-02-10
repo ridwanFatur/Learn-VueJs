04e154f91d6250f13b123e427c154c2f88abd638
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-10 22:33:15 +0700

    updatE

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
+import User from '@/views/User.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
+	{
+		path: '/',
+		name: 'Home',
+		component: Home
+	},
+	{
+		path: '/about',
+		name: 'About',
 		component: About
-  }
+	},
+	{
+		path: '/user/:id',
+		name: 'User',
+		component: User,
+		props: true,
+	},
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+	history: createWebHistory(process.env.BASE_URL),
+	routes
 })
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <button @click="navAbout">About</button>
+    <button @click="navUser">User</button>
+    <button @click="replaceUser">Replace User</button>
+    <button @click="navBack">Back</button>
   </div>
 </template>
-
 <script>
-
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+  methods: {
+    navAbout() {
+      console.log("About");
+      this.$router.push("/about");
+    },
+    navUser() {
+      const username = "Ridwan";
+      console.log("User");
+      this.$router.push(`/user/${username}`);
+    },
+    replaceUser() {
+      const username = "Ridwan";
+      console.log("User Replace");
+      this.$router.replace(`/user/${username}`);
+    },
+    navBack() {
+      this.$router.back();
+    },
+  },
+};
 </script>
+<style></style>
