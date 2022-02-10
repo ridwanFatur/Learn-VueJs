40eac6be412a2e0a9f9ad7070eef2daff1402492
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-10 22:00:52 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,48 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
+import User from '@/views/User.vue'
+import UserProfile from '@/views/UserProfile.vue'
+import UserPosts from '@/views/UserPosts.vue'
+import UserEmpty from '@/views/UserEmpty.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
+	{
+		path: '/',
+		component: Home
+	},
+	{
+		path: '/about',
 		component: About
-  }
+	},
+	{
+		path: '/user/:id',
+		component: User,
+		props: true,
+		children:[
+			{
+        path: '',
+        component: UserEmpty,
+      },
+			{
+        // UserProfile will be rendered inside User's <router-view>
+        // when /user/:id/profile is matched
+        path: 'profile',
+        component: UserProfile,
+      },
+      {
+        // UserPosts will be rendered inside User's <router-view>
+        // when /user/:id/posts is matched
+        path: 'posts',
+        component: UserPosts,
+      },
+		]
+	}
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+	history: createWebHistory(process.env.BASE_URL),
+	routes
 })
 
 export default router
---
+++ src/views/User.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>User {{ id }}</h2>
+  <router-view></router-view>
+</template>
+<script>
+export default {
+  name: "User",
+  props: ["id"],
+  data() {
+    return {};
+  },
+};
+</script>
+<style></style>
