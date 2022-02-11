9dfa1771646e6c34faa3a45477936f2631bc2908
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-11 08:39:25 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,49 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
+import Search from '@/views/Search.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
+	{
+		path: '/',
+		name: 'Home',
+		component: Home
+	},
+	{
+		path: '/about',
+		name: 'About',
+		alias: ["/about-me", "/about-us"],
 		component: About
-  }
+	},
+	{ path: '/home', redirect: '/' },
+	{
+		// /search/screens -> /search?q=screens
+		path: '/search/:searchText',
+		redirect: to => {
+			// the function receives the target route as the argument
+			// we return a redirect path/location here.
+			return { path: '/search', query: { q: to.params.searchText } }
+		},
+	},
+	{
+		path: '/search',
+		component: Search
+	},
+	{
+		// will always redirect /users/123/posts to /users/123/profile
+		path: '/users/:id/posts',
+		redirect: to => {
+			// the function receives the target route as the argument
+			// a relative location doesn't start with `/`
+			// or { path: 'profile'}
+			return 'about'
+		},
+	},
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+	history: createWebHistory(process.env.BASE_URL),
+	routes
 })
 
 export default router
---
+++ src/views/Search.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Search "{{this.$route.query.q}}"</div>
+</template>
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {};
+  },
+};
+</script>
+<style></style>
