510aa6ad3d1016e26c1a75f29a13795dcc782723
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-11 09:21:03 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
-import Post from '@/views/Post.vue'
-import PostBeforeNavigation from '@/views/PostBeforeNavigation.vue'
 
 const routes = [
 	{
@@ -17,15 +15,6 @@ const routes = [
 		component: About
 	},
 	{ path: '/home', redirect: '/' },
-	// AFTER NAVIGATION
-	{
-		path: '/post/:id',
-		component: Post
-	},
-	{
-		path: '/post-before/:id',
-		component: PostBeforeNavigation
-	},
 ]
 
 const router = createRouter({
