f6d6aa6fc29d7e965394f2e23f844a5e6c783868
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-11 13:57:58 +0700

    update

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,19 @@
+<template>
+  <h3>{{ post.title }}</h3>
+	<!-- <h1>Test</h1> -->
+  <div v-html="post.content"></div>
+</template>
+
+<script>
+export default {
+  props: ["post"],
+  data: function () {
+    return {};
+  },
+	created(){
+		// console.log("Test")
+		// console.log(this.post)
+		
+	}
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <ButtonCounter />
+    <ButtonCounter />
+    <ButtonCounter />
+    <!-- <blog-post title="My journey with Vue" /> -->
+		<blog-post v-bind:post="myPost"/>
+    <blog-post
+      v-for="post in posts"
+      v-bind:key="post.id"
+      v-bind:post="post"
+    />
   </div>
 </template>
 
 <script>
+import ButtonCounter from "@/components/ButtonCounter";
+import BlogPost from "@/components/BlogPost";
 
+export default {
+  components: {
+    ButtonCounter: ButtonCounter,
+    "blog-post": BlogPost,
+  },
+  data() {
+    return {
+			myPost: {id: 3, title: "Test----", content: "G----"},
+      posts: [
+        { id: 1, title: "My journey with Vue", content: "Ridwan Content" },
+        { id: 2, title: "Blogging with Vue", content: "Fatur Content" },
+      ],
+    };
+  },
+};
 </script>
