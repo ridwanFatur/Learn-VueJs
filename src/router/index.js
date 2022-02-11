9cd990cf1397bc92273ebd86b0c6c82d058ae8ed
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-11 08:45:58 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
-import Search from '@/views/Search.vue'
 
 const routes = [
 	{
@@ -16,29 +15,6 @@ const routes = [
 		component: About
 	},
 	{ path: '/home', redirect: '/' },
-	{
-		// /search/screens -> /search?q=screens
-		path: '/search/:searchText',
-		redirect: to => {
-			// the function receives the target route as the argument
-			// we return a redirect path/location here.
-			return { path: '/search', query: { q: to.params.searchText } }
-		},
-	},
-	{
-		path: '/search',
-		component: Search
-	},
-	{
-		// will always redirect /users/123/posts to /users/123/profile
-		path: '/users/:id/posts',
-		redirect: to => {
-			// the function receives the target route as the argument
-			// a relative location doesn't start with `/`
-			// or { path: 'profile'}
-			return 'about'
-		},
-	},
 ]
 
 const router = createRouter({
