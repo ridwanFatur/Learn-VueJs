d419d934513b1f0c8445c4ce4123f1691fd8866a
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-15 13:58:20 +0700

    update

--- src/main.js
+++ src/main.js
@@ -4,16 +4,31 @@ import router from './router'
 import { createStore } from 'vuex'
 
 const store = createStore({
-  state () {
-    return {
-      count: 0
-    }
-  },
-  mutations: {
-    increment (state) {
-      state.count++
-    }
-  }
+	state() {
+		return {
+			count: 0,
+			todos: [
+				{ id: 1, text: 'Ridwan', done: true },
+				{ id: 2, text: 'Fatur', done: false }
+			]
+		}
+	},
+	getters: {
+		doneTodos(state) {
+			return state.todos.filter(todo => todo.done)
+		},
+		doneTodosCount(state, getters) {
+			return getters.doneTodos.length
+		},
+		getTodoById: (state) => (id) => {
+			return state.todos.find(todo => todo.id === id)
+		}
+	},
+	mutations: {
+		increment(state) {
+			state.count++
+		}
+	}
 })
 
 createApp(App).use(router).use(store).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,22 +3,48 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <button v-on:click="testing">Test</button>
     <button v-on:click="increment">Increment</button>
+    <button v-on:click="print">Print todo done</button>
+    <button v-on:click="testMapGetter">Test Map Getter</button>
   </div>
   <p>Count: {{ this.$store.state.count }}</p>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   data() {
     return {};
   },
+  computed: {
+    ...mapGetters(["doneTodosCount", "doneTodos"]),
+    // ...mapGetters({
+    // 	doneCount: 'doneTodosCount',
+    // 	doneList: 'doneTodos',
+    // })
+  },
   methods: {
+    testMapGetter: function () {
+      // console.log(this.doneCount);
+      // console.log(this.doneList);
+			console.log(this.doneTodosCount);
+      console.log(this.doneTodos);
+    },
     testing: function () {
       console.log(this.$store.state.count);
       console.log("Testing");
     },
     increment: function () {
       this.$store.commit("increment");
+    },
+    print: function () {
+      console.log("Print");
+      const listDoneTodos = this.$store.getters.doneTodos;
+      console.log(listDoneTodos);
+      console.log(listDoneTodos[0].text);
+      console.log(this.$store.getters.doneTodosCount);
+
+      console.log(this.$store.getters.getTodoById(2));
     },
   },
 };
