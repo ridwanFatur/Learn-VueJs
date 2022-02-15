0170ec90f24c3a57319d6098f0f2b757d12693a1
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-15 13:18:18 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createStore } from 'vuex'
 
-createApp(App).use(router).mount('#app')
+const store = createStore({
+  state () {
+    return {
+      count: 0
+    }
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
+createApp(App).use(router).use(store).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <button v-on:click="testing">Test</button>
+    <button v-on:click="increment">Increment</button>
   </div>
+  <p>Count: {{ this.$store.state.count }}</p>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    testing: function () {
+      console.log(this.$store.state.count);
+      console.log("Testing");
+    },
+    increment: function () {
+      this.$store.commit("increment");
+    },
+  },
+};
 </script>
