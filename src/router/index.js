ba427fbaa7665634f73e19bd79807f59cae3ec60
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-15 15:17:13 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
 import MyScreen from '@/views/MyScreen.vue'
+import axios from 'axios';
 
 const routes = [
 	{
@@ -26,5 +27,16 @@ const router = createRouter({
 	history: createWebHistory(process.env.BASE_URL),
 	routes
 })
+
+// router.beforeEach(async (to, from, next) => {
+// 	if (to.path == "/my-screen") {
+// 		const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
+// 		console.log(response)
+// 		console.log("My Screen")
+// 		next()
+// 	} else {
+// 		next()
+// 	}
+// });
 
 export default router
