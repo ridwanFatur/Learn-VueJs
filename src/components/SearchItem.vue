424a594eac41b869a953d89dd31b47b92f0e3f21
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-20 17:28:34 +0700

    update

--- src/components/SearchItem.vue
+++ src/components/SearchItem.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <p>URL</p>
-    <p>Title</p>
-    <p>Description</p>
+    <p class="url">{{ data.url }}</p>
+    <a class="title" v-bind:href="data.url" target="_blank">{{ data.title }}</a>
+    <p class="description">{{ data.desc }}</p>
   </div>
 </template>
 
 <script>
 export default {
-	props: ["data"],
+  props: ["data"],
   data() {
     return {};
   },
@@ -16,7 +16,45 @@ export default {
 </script>
 
 <style scoped>
-p {
+p,
+label,
+a {
   margin: 0;
+}
+
+.url {
+  font-size: 12px;
+  color: black;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+}
+
+.title {
+  font-size: 25px;
+  color: blue;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+}
+
+.title:hover {
+  color: darkblue;
+  cursor: pointer;
+}
+
+.description {
+  font-size: 14px;
+  text-align: justify;
+  color: darkgrey;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,59 @@
 <template>
   <div>
     <b-container>
-      <p>Sekitar {{ searchData.count }} hasil ({{ searchData.timeSeach }} detik)</p>
-			<p>Results</p>
-			<search-item/>
+      <template v-if="searchData.listData.length > 1">
+        <p>
+          Sekitar {{ searchData.listData.length }} hasil ({{
+            searchData.timeSeach
+          }}
+          detik)
+        </p>
+        <p>Results</p>
+
+        <template v-for="data in searchData.listData">
+          <b-row v-bind:key="data.id" class="mb-4">
+            <b-col lg="6">
+              <search-item v-bind:data="data" />
+            </b-col>
+          </b-row>
+        </template>
+      </template>
     </b-container>
+
+    <template v-for="i in 10">
+      <div :key="i" class="my-box mb-2">
+        <p>{{ i }}</p>
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
 import SearchItem from "@/components/SearchItem";
+import { descriptionText } from "@/constants/descriptionConstants";
 
 export default {
-	components:{
-		"search-item": SearchItem
-	},
+  components: {
+    "search-item": SearchItem,
+  },
   data() {
     return {
       searchData: {
-        count: 10,
         timeSeach: 0.38,
-				data: [
-					{
-						"url": "https://www.facebook.com/",
-						"title": "Facebook",
-						"desc": "The most interesting app in the world",
-					},
-				],
+        listData: [
+          // {
+          //   url: "https://www.facebook.com/",
+          //   title: "Facebook",
+          //   desc: descriptionText,
+          //   id: 1,
+          // },
+          // {
+          //   url: "https://www.google.com/",
+          //   title: "Google",
+          //   desc: descriptionText,
+          //   id: 2,
+          // },
+        ],
       },
     };
   },
@@ -35,5 +62,17 @@ export default {
 <style scoped>
 p {
   font-size: 14px;
+}
+
+.my-box p {
+  color: white;
+  font-size: 30px;
+  text-align: center;
+}
+
+.my-box {
+  width: 100%;
+  height: 200px;
+  background-color: red;
 }
 </style>
