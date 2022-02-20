2e51f020dde0679c44a2fb98042af39e4770a660
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-20 17:29:38 +0700

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,15 @@
 <template>
   <div>
-    <b-container>
-      <p>Sekitar {{ searchData.count }} hasil ({{ searchData.timeSeach }} detik)</p>
-			<p>Results</p>
-			<search-item/>
-    </b-container>
+    <b-container> 
+			<p>Home</p>
+		</b-container>
   </div>
 </template>
 
 <script>
-import SearchItem from "@/components/SearchItem";
-
 export default {
-	components:{
-		"search-item": SearchItem
-	},
   data() {
-    return {
-      searchData: {
-        count: 10,
-        timeSeach: 0.38,
-				data: [
-					{
-						"url": "https://www.facebook.com/",
-						"title": "Facebook",
-						"desc": "The most interesting app in the world",
-					},
-				],
-      },
-    };
+    return {};
   },
 };
 </script>
