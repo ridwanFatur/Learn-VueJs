65b58864566676cbca60e0323227f2fa54de472d
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-20 13:20:55 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
+const bootstrapSassAbstractsImports = require('vue-cli-plugin-bootstrap-vue/sassAbstractsImports.js')
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+	css: {
+		loaderOptions: {
+			sass: {
+				additionalData: bootstrapSassAbstractsImports.join('\n')
+			},
+			scss: {
+				additionalData: [...bootstrapSassAbstractsImports, ''].join(';\n')
+			}
+		}
+	}
 }
